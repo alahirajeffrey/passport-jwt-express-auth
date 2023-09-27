f11214956d957eaadcc4897615fb1c86bd2a1dcf
alahirajeffrey <alahirajeffrey@gmail.com>
2023-09-27 22:57:35 +0100

    added user model

---
+++ src/model.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+const UserModel = mongoose.model("user", UserSchema);
+
+module.exports = UserModel;
